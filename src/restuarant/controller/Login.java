45c6ec6b90d87a9badcdd3ce775a8051acfae133
Kanniphit Buawan-ngam <toeyarmy@hotmail.com>
2017-11-24 22:29:43 +0700

    build login method

--- src/restuarant/controller/Login.java
+++ src/restuarant/controller/Login.java
@@ -5,10 +5,37 @@
  */
 package restuarant.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author taloey
  */
 public class Login {
-    
+
+    public boolean LoginRestuarant(String username, String password) {
+        boolean status = false;
+        String passworduser = null;
+        try {
+            Connection con = ConnectionBuilder.getConnection();
+            String sql = "select * from User where Username = ?";
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setString(1, username);
+            ResultSet rs = pstm.executeQuery();
+
+            if (rs.next()) {
+                passworduser = rs.getString("Password");
+            }
+            if(password.equals(passworduser)){
+                status= true;
+            }
+            con.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return status;
+    }
 }
